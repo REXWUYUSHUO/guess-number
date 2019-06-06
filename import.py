009ee643ba438cf63f58520567261b83ec0d087f
WuYushuo <33063812+REXWUYUSHUO@users.noreply.github.com>
2019-06-06 18:13:04 +0800

    Add files via upload

--- import.py
+++ import.py
@@ -1,7 +1,10 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+    count = count + 1
+
     num = input('請猜數字:')
     num = int(num)
     if num == r:
@@ -11,4 +14,4 @@ while True:
         print('比答案大要猜小一點')
     elif num < r:
         print('比答案小要大一點')
-
+    print('這是你猜的第', count, '次')
